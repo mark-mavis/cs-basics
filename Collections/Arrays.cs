63978b0ea0922dc2c0ba7d01eaa2e3a4695adc0f
Mark Mavis <mmavis@uw.edu>
2022-11-09 00:31:39 -0800

    Add Arrays

--- Collections/Arrays.cs
+++ Collections/Arrays.cs
@@ -4,9 +4,37 @@ namespace Collections
 {
     class Array
     {
-        public static void RunTwoDimensional()
+        public static void SingleDimensionalArray_RunExercise()
         {
+            int[] arr = new int[5];
+            string[] strarr = new string[2] { "Hello","yes"};
+            
+            //2D String Matrix
+            string[,] Array2D = new string[,] 
+            { 
+                {"1", "2"}, 
+                {"3", "4"} 
+            };
 
+            //3D String 
+            string[,,] Array3D = new string[,,]
+            {
+                {
+                    {"000", "010", "020" },
+                    {"100", "110", "120" },
+                    {"200", "210", "220" }
+                },
+                {
+                    {"001", "011", "021" },
+                    {"101", "111", "121" },
+                    {"201", "211", "221" }
+                },
+                {
+                    {"003", "013", "023" },
+                    {"103", "113", "123" },
+                    {"203", "213", "223" }
+                }
+            };
         }
     }
 }
