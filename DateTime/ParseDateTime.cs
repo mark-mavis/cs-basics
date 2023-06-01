e5e7b176a76002e1351576198fff354b03ca47b5
markmavis <mark.mavis+github@outlook.com>
2023-06-01 00:18:52 -0700

    Add DateTime Parse Challenge

--- DateTime/ParseDateTime.cs
+++ DateTime/ParseDateTime.cs
@@ -5,22 +5,29 @@ namespace ParseDateTime;
 
 public class ParseInput{
     public DateTime today;
+    private string CalculateDays(DateTime dt1, DateTime dt2){
+        if(dt1 > dt2) return $"Happens in {(dt1-dt2).Days} days";
+        else if (dt2 > dt1) return $"Happened {(dt2-dt1).Days} days ago";
+        else return $"That is today!";
+    }
     public void Initialize(){
         
         today = DateTime.Today;
+        DateTime dt;
+        string? str_dt;
 
-        Console.WriteLine($"Enter a date...");
-
-        DateTime input_date;
-
-        if(DateTime.TryParse(Console.ReadLine(), out input_date)){
-            if(input_date == today){
-                Console.WriteLine($"is today!");
+        do{
+            Console.WriteLine($"Enter a date... (or 'exit')");
+            str_dt = Console.ReadLine();
+            if(str_dt.ToLower() == "exit"){
+                break;
             }
-            TimeSpan ts = input_date > DateTime.Now ? input_date - DateTime.Now : DateTime.Now - input_date;
-        }
 
-
-
+            if(DateTime.TryParse(str_dt, out dt)){
+                Console.WriteLine(CalculateDays(dt, today));
+            }else{
+                Console.WriteLine("That doesn't seem to be a valid date");
+            }
+        }while(str_dt != "exit");
     }
 }
